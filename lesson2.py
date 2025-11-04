f511a2ed7ccc7194a1980222ab276946c1120ebd
arziwbektemirxanov4-arch <arziwbektemirxanov4@gmail.com>
2025-11-04 09:15:22 +0500

    code

---
+++ lesson2.py
@@ -0,0 +1,49 @@
+# Varabelss - Ozgeriwshiler
+
+# a = "Hello World!"
+# print(a)
+
+
+# a = 15
+# b = 4
+# c = a + b
+# print(c)  # Output: 19
+
+
+
+# Data Types - MaglumaT turleri
+# x = 5               # Integer
+# y = 3.14            # Float
+# z = "Hello"        # String
+# is_active = True    # Boolean
+# print(type(x))      # Output: <class 'int'>
+# print(type(y))      # Output: <class 'float'>
+# print(type(z))      # Output: <class 'str'>
+
+
+# Type Conversion - MaglumaT turlerinin ozgeriwi
+# x = 5               # Integer 
+# y = float(x)       # Convert to Float
+# z = str(x)         # Convert to String
+# print(type(y))      # Output: <class 'float'>
+# print(type(z))      # Output: <class 'str'>
+
+
+# String Manipulation - String metodlari
+text = "Hello World!"
+# print(text.upper())      # Output: "HELLO, WORLD!"
+# print(text.lower())      # Output: "hello, world!"
+# print(text.replace("World", "Python"))  # Output: "Hello, Python!"
+# print(text.split(", "))  # Output: ['Hello', 'World!']
+# print(text[7:12])       # Output: "World"
+# print(len(text))        # Output: 13
+
+# String Formatting - Formatlaw
+# name = "Math"
+# greeting = f"Hello {name}!"  # F-string
+# print(greeting)          # Output: "Hello, Alice!"
+# user = "John"
+# welcome_message = "Welcome {}".format(user)  # format() method
+# print(welcome_message)   # Output: "Welcome, Bob!"
+
+
