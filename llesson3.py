8652ef7f26e5b1bb35d005bac2dac072309552b1
arziwbektemirxanov4-arch <arziwbektemirxanov4@gmail.com>
2025-11-04 09:34:13 +0500

    code

---
+++ llesson3.py
@@ -0,0 +1,11 @@
+# listler menen islesiw
+
+a = [1, 2, 3, 4, 5]
+# print(a)                 # Output: [1, 2, 3, 4, 5]
+# print(a[0])              # Output: 1
+
+
+# List metodlari
+
+a.append(10)
+print(a)                 # Output: [1, 2, 3, 4, 5, 10]
